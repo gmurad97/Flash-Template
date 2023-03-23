4eec78c0664a8a0e67748c38372d13afba128a90
gmurad97 <murad.dev@bk.ru>
2023-03-23 05:38:41 +0400

    release

---
+++ js/typing-module.js
@@ -0,0 +1,50 @@
+const wordsArray = ["Logistic", "Freight", "Brokerage"];
+let wordsIndex = 0;
+let isDeleting = false;
+let tempText = "";
+
+/*BEGIN - CONTROL*/
+let textElement = document.getElementById("typing-text");
+
+const SPEED_TYPING_TEXT = 128;
+const WAIT_TYPING_TEXT = 1536;
+const SPEED_DELETING_TEXT = 128;
+const WAIT_DELETING_TEXT = 128;
+/*ENDED - CONTROL*/
+
+function typingText() {
+    const currentWord = wordsArray[wordsIndex];
+    if (!isDeleting) {
+        tempText = currentWord.substring(0, tempText.length + 1);
+        textElement.innerHTML = tempText;
+        if (tempText === currentWord) {
+            isDeleting = true;
+            setTimeout(() => {
+                typingText();
+            }, WAIT_TYPING_TEXT);
+        }
+        else {
+            setTimeout(() => {
+                typingText();
+            }, SPEED_TYPING_TEXT);
+        }
+    }
+    else {
+        tempText = currentWord.substring(0, tempText.length - 1);
+        textElement.innerHTML = tempText;
+        if (tempText === "") {
+            isDeleting = false;
+            wordsIndex = (wordsIndex + 1) % wordsArray.length;
+            setTimeout(() => {
+                typingText();
+            }, WAIT_DELETING_TEXT);
+        }
+        else {
+            setTimeout(() => {
+                typingText();
+            }, SPEED_DELETING_TEXT);
+        }
+    }
+}
+
+typingText();
