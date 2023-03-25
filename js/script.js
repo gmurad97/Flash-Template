9e481646c045c1ee0726d6c2356b2830e66fc566
gmurad97 <murad.dev@bk.ru>
2023-03-26 03:45:03 +0400

    release

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 new WOW().init();
+
+function faBounceEnabled(elmRoot){
+    elmRoot.querySelector(".fa-flash-second-type").classList.add("fa-bounce");
+}
+
+function faBounceDisabled(elmRoot){
+    elmRoot.querySelector(".fa-flash-second-type").classList.remove("fa-bounce");
+}
